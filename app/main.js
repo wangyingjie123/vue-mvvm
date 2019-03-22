354a4fbb64518dc3ec77a236aa930313a3db45ad
wangyingjie07 <wangyingjie07@baidu.com>
2019-03-22 21:54:44 +0800

    mvvm

--- app/main.js
+++
@@ -1,7 +0,0 @@
-//main.js
-import React from 'react';
-import {render} from 'react-dom';
-import Greeter from './Greeter'; // react
-import './main.css';//使用require导入css文件,全局
-
-render(<Greeter />, document.getElementById('root'));
